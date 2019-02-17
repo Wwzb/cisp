db9b8ad74005db70546f00561b938e67e69fa0b1
Zbin <wuzhibin@qipeng.com>
2019-02-17 13:16:04 +0800

    添加登录检测过滤器

--- src/main/java/com/zbin/cisp/MyConfig.java
+++ src/main/java/com/zbin/cisp/MyConfig.java
@@ -10,8 +10,11 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class MyConfig implements WebMvcConfigurer {
 
+  //配置静态资源路径
   @Override
   public void addResourceHandlers(ResourceHandlerRegistry registry) {
     registry.addResourceHandler("/static/**").addResourceLocations("classpath:/static/");
   }
+
+
 }
